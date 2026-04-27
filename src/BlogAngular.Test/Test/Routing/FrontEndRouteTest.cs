3ec93e8f79670031bc32be267fd6e5347f61b3ba
cioina <alexei_cioina@yahoo.com>
2026-04-27 09:01:15 -0700

    update all

--- src/BlogAngular.Test/Test/Routing/FrontEndRouteTest.cs
+++ src/BlogAngular.Test/Test/Routing/FrontEndRouteTest.cs
@@ -1,9 +1,6 @@
 ﻿using BlogAngular.Application.Common.Version;
 using BlogAngular.Web.Features;
-//using MediatR;
-//using Microsoft.Extensions.DependencyInjection;
 using MyTested.AspNetCore.Mvc;
-//using MyTested.AspNetCore.Mvc.Internal.Services;
 using Xunit;
 
 namespace BlogAngular.Test.Routing
@@ -17,7 +14,6 @@ namespace BlogAngular.Test.Routing
         .ShouldMap(request => request
             .WithMethod(HttpMethod.Get)
             .WithLocation("api/v1.0/version"))
-        //.To<VersionController>(c => c.Index(TestServiceProvider.Current.GetService<IMediator>()!))
         .To<VersionController>(c => c.Index())
         .Which()
         .ShouldReturn()
--- src/BlogAngular.Web/Web/Features/TagsController.cs
+++ src/BlogAngular.Web/Web/Features/TagsController.cs
@@ -4,52 +4,14 @@ using BlogAngular.Application.Blog.Tags.Commands.Delete;
 using BlogAngular.Application.Blog.Tags.Commands.Edit;
 using BlogAngular.Application.Blog.Tags.Queries.Listing;
 using BlogAngular.Application.Common;
-//using BlogAngular.Web.Infrastructure;
 using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Authorization.Infrastructure;
-//using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
-//using System.Security.Claims;
 using System.Threading.Tasks;
 using static BlogAngular.Domain.Common.Models.ModelConstants.Identity;
 
 namespace BlogAngular.Web.Features
 {
-    //public class TagsController : EndpointGroupBase
-    //{
-    //    public override string GroupName => "tags";
-    //    public override void Map(RouteGroupBuilder groupBuilder)
-    //    {
-    //        var authorizationPolicy = new AuthorizationPolicy([new DenyAnonymousAuthorizationRequirement(),
-    //            new ClaimsAuthorizationRequirement(ClaimTypes.NameIdentifier, null),
-    //            new RolesAuthorizationRequirement([AdministratorRoleName])], [Bearer]);
-
-    //        groupBuilder.MapGetIndex(Tags);
-    //        groupBuilder.MapPost(Create, nameof(Create)).RequireAuthorization([BearerPolicy]);
-    //        groupBuilder.MapPut(Edit, nameof(Edit) + PathSeparator + Id).RequireAuthorization(authorizationPolicy);
-    //        groupBuilder.MapDelete(Delete, nameof(Delete) + PathSeparator + Id).RequireAuthorization(authorizationPolicy);
-    //    }
-    //    //https://github.com/dotnet/aspnetcore/issues/55719
-    //    public async Task<ActionResult<TagsResponseEnvelope>> Tags(
-    //        [AsParameters] TagsQuery query)
-    //        => await this.Send(query);
-
-    //    public async Task<ActionResult<TagResponseEnvelope>> Create(
-    //        [FromBody] TagCreateCommand command)
-    //        => await this.Send(command);
-
-    //    public async Task<ActionResult<TagResponseEnvelope>> Edit(
-    //        [FromRoute] int id,
-    //        [FromBody] TagEditCommand command)
-    //        => await this.Send(command.SetId(id));
-
-    //    public async Task<ActionResult<int>> Delete(
-    //        [FromBody] TagDeleteCommand command)
-    //        => await this.Send(command);
-    //}
-
     public class TagsController : ApiController
     {
         [HttpGet]
